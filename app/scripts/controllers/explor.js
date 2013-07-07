7f1b864658f2947bec3d2e713dd50f54f70fdb79
whatevercode <florian.gr@gmx.net>
2013-07-07 21:36:25 +0200

    added explor controller and view

---
+++ app/scripts/controllers/explor.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('elastularApp')
+  .controller('ExplorCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
